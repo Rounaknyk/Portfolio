1f14ae2d6c32fb1abd76ce79e38d0b05062cefcb
Rounaknyk <rounakpro18@gmail.com>
2026-03-26 21:27:39 +0530

    added 3D model of globe

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -17,7 +17,8 @@ function Marker({ name, lat, lng }: { name: string, lat: number, lng: number }) 
   
   const phi = (90 - lat) * (Math.PI / 180);
   const theta = (lng + 180) * (Math.PI / 180);
-
+ 
+  
   const x = -(radius * Math.sin(phi) * Math.cos(theta));
   const z = (radius * Math.sin(phi) * Math.sin(theta));
   const y = (radius * Math.cos(phi));
